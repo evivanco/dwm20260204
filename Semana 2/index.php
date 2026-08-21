38c4571bf6e63abc2c41b6e1aff91c31b0da02a8
Ernesto Vivanco <eduardo.vivanco@gmail.com>
2026-08-21 13:31:13 -0400

    DWM S3 Actualización Bootstrap Ejemplos Nuevos

--- Semana 2/index.php
+++ Semana 2/index.php
@@ -1,90 +1,84 @@
-<!DOCTYPE html>
-<html lang="es">
+<html>
     <head>
         <title>Pagina Principal</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>         
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>        
     </head>
     <body>
-        <!-- Navbar -->
+        <!--Navbar -->
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.php">Logo</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
-                </button>          
-                <div class="collapse navbar-collapse" id="collapsibleNavbar">      
+                </button> 
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Empresa</a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="empresa.php">Nuestra Empresa</a></li>
+                                <li><a class="dropdown-item" href="empresa.php">Quienes Somos</a></li>
                                 <li><a class="dropdown-item" href="#">Nuestro Equipo</a></li>
                                 <li><a class="dropdown-item" href="#">Mision</a></li>
                             </ul>
                         </li>                        
                         <li class="nav-item">
-                            <a class="nav-link" href="productos.php">Productos</a>
-                        </li>
-                        <li class="nav-item">
                             <a class="nav-link" href="servicios.php">Servicios</a>
                         </li>
                         <li class="nav-item">
+                            <a class="nav-link" href="productos.php">Productos</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="contacto.php">Contacto</a>
-                        </li> 
+                        </li>                           
                     </ul>
-                </div>
-                <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#myModal">Acceder</button>
+                </div>  
+                <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#myModal">Acceder</button>                             
             </div>
         </nav>
-        <!-- Container -->
+        <!--Container-->
         <div class="container-fluid bg-warning">
-            Pagina Principal<br>
-            <a href="index.php">Ir a Principal</a><br>
             <a href="empresa.php">Ir a Empresa</a><br>
-            <a href="productos.php">Ir a Productos</a><br>
             <a href="servicios.php">Ir a Servicios</a><br>
-            <a href="contacto.php">Ir a Contacto</a>
+            <a href="productos.php">Ir a Producto</a><br>
+            <a href="Contacto.php">Ir a Contacto</a><br>
         </div>
-        <!-- Footer -->
+        <!--Footer-->
         <div class="container-fluid bg-dark">
             <div class="row">
                 <div class="col-4"></div>
-                <div class="col-4 d-flex justify-content-center" 
-                style="color:white"><strong>MiEmpresa@2026</strong></div>
+                <div class="col-4" style="color:white"><strong>MiEmpresa@2026</strong></div>
                 <div class="col-4"></div>
             </div>
         </div>
-
-        <!-- Modal -->
+        <!--Modal-->
         <div class="modal fade" id="myModal">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <!-- Modal Header -->
                     <div class="modal-header">
-                        <h4 class="modal-title">Autenticar</h4>
+                        <h4 class="modal-title">Autenticacion</h4>
                         <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                     </div>
                     <!-- Modal body -->
                     <div class="modal-body">
                         <form action="empresa.php">
-                            <div class="mb-2 mt-2">
+                            <div class="mb-3 mt-3">
                                 <label for="email" class="form-label">Email:</label>
                                 <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                             </div>
-                            <div class="mb-2">
+                            <div class="mb-3">
                                 <label for="pwd" class="form-label">Password:</label>
                                 <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
                             </div>
-                            <div class="form-check mb-2">
+                            <div class="form-check mb-3">
                                 <label class="form-check-label">
                                 <input class="form-check-input" type="checkbox" name="remember"> Remember me
                                 </label>
                             </div>
-                            <button type="submit" class="btn btn-primary">Login <i class="fa fa-check-circle"></i></button>
+                            <button type="submit" class="btn btn-primary">Login</button>
                         </form>
                     </div>
                     <!-- Modal footer -->
@@ -93,6 +87,6 @@
                     </div>
                 </div>
             </div>
-        </div>         
+        </div>        
     </body>
 </html>
--- Semana 2/productos.php
+++ Semana 2/productos.php
@@ -1,124 +1,54 @@
-<!DOCTYPE html>
-<html lang="es">
+<html>
     <head>
         <title>Pagina Principal</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>         
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>        
     </head>
     <body>
-        <!-- Navbar -->
+        <!--Navbar -->
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.php">Logo</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
-                </button>          
-                <div class="collapse navbar-collapse" id="collapsibleNavbar">      
+                </button> 
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Empresa</a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="empresa.php">Nuestra Empresa</a></li>
+                                <li><a class="dropdown-item" href="empresa.php">Quienes Somos</a></li>
                                 <li><a class="dropdown-item" href="#">Nuestro Equipo</a></li>
                                 <li><a class="dropdown-item" href="#">Mision</a></li>
                             </ul>
                         </li>                        
                         <li class="nav-item">
-                            <a class="nav-link" href="productos.php">Productos</a>
-                        </li>
-                        <li class="nav-item">
                             <a class="nav-link" href="servicios.php">Servicios</a>
                         </li>
                         <li class="nav-item">
+                            <a class="nav-link" href="productos.php">Productos</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="contacto.php">Contacto</a>
-                        </li> 
+                        </li>
                     </ul>
-                </div>
-                <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#myModal">Acceder</button>
+                </div>                               
             </div>
         </nav>
-        <!-- Container -->
+        <!--Container-->
         <div class="container-fluid bg-warning">
-            <div class="container">
-                <div id="demo" class="carousel slide" data-bs-ride="carousel">
-
-                <!-- Indicators/dots -->
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-                    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-                    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-                </div>
-
-                <!-- The slideshow/carousel -->
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img src="img/la.jpg" alt="Los Angeles" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                    <img src="img/chicago.jpg" alt="Chicago" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                    <img src="img/ny.jpg" alt="New York" class="d-block w-100">
-                    </div>
-                </div>
-
-                <!-- Left and right controls/icons -->
-                <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon"></span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-                    <span class="carousel-control-next-icon"></span>
-                </button>
-                </div>                
-            </div>
+            <a href="index.php">Volver</a>
         </div>
-        <!-- Footer -->
+        <!--Footer-->
         <div class="container-fluid bg-dark">
             <div class="row">
                 <div class="col-4"></div>
-                <div class="col-4 d-flex justify-content-center" 
-                style="color:white"><strong>MiEmpresa@2026</strong></div>
+                <div class="col-4" style="color:white"><strong>MiEmpresa@2026</strong></div>
                 <div class="col-4"></div>
             </div>
         </div>
-
-        <!-- Modal -->
-        <div class="modal fade" id="myModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <!-- Modal Header -->
-                    <div class="modal-header">
-                        <h4 class="modal-title">Autenticar</h4>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                    </div>
-                    <!-- Modal body -->
-                    <div class="modal-body">
-                        <form action="empresa.php">
-                            <div class="mb-2 mt-2">
-                                <label for="email" class="form-label">Email:</label>
-                                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-                            </div>
-                            <div class="mb-2">
-                                <label for="pwd" class="form-label">Password:</label>
-                                <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
-                            </div>
-                            <div class="form-check mb-2">
-                                <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox" name="remember"> Remember me
-                                </label>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Login <i class="fa fa-check-circle"></i></button>
-                        </form>
-                    </div>
-                    <!-- Modal footer -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>         
+        <!--Modal-->
     </body>
 </html>
